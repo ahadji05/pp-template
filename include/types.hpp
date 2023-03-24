0e1a781ef50b644365435ffad04cf418c5a15ae8
ahadji05 <andreashadji1995@gmail.com>
2023-03-24 13:24:21 +0200

    header file for the definition of global data-types.

--- include/types.hpp
+++ include/types.hpp
@@ -1,9 +1,12 @@
+// GENERAL INCLUDES
 #include "assert.h"
-#include "memory/MemorySpacesInc.hpp"
 
+// SELECT FLOATING-POINT PRECISION (DATA-TYPE)
 #include "half.hpp"
 using float_type = half_float::half;
 
+// SELECT MEMORY-SPACE BASED ON COMPILE-TIME CHOICE
+#include "memory/MemorySpacesInc.hpp"
 #if defined(TMP_ENABLE_CUDA_BACKEND)
 using memo_space = TMP::MemSpaceCuda;
 #elif defined(TMP_ENABLE_HIP_BACKEND)
@@ -11,3 +14,7 @@ using memo_space = TMP::MemSpaceHip;
 #else
 using memo_space = TMP::MemSpaceHost;
 #endif
+
+// DEFINE VECTOR-TYPE vector_type based on: memo_space and float_type
+#include "containers/Vector.hpp"
+using vector_type = TMP::Vector<float_type, memo_space>;
