2ee01031a3626d672c9da4bb6ba0f6cabcfd5d29
ahadji05 <andreashadji1995@gmail.com>
2023-03-28 11:06:23 +0300

    introduced execution-spaces: Serial, OpenMP, Cuda, and Hip.

---
+++ include/execution/ExecutionSpaceCuda.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * @author  Andreas Hadjigeorgiou, The Cyprus Institute,
+ *          Personal-site: https://ahadji05.github.io,
+ *          E-mail: a.hadjigeorgiou@cyi.ac.cy
+ *
+ * @copyright 2022 CaSToRC (The Cyprus Institute), Delphi Consortium (TU Delft)
+ *
+ * @version 1.0
+ *
+ * @section LICENCE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef TMP_EXECUTION_SPACE_CUDA
+#define TMP_EXECUTION_SPACE_CUDA
+
+#include "execution/ExecutionSpace.hpp"
+#include "memory/MemorySpacesInc.hpp"
+
+namespace TMP
+{
+
+class ExecutionSpaceCuda : public ExecutionSpaceBase
+{
+  public:
+    using accessible_space = TMP::MemSpaceCuda; // define as type-trait the accessible memory-space
+};
+
+} // namespace TMP
+
+#endif
---
+++ include/execution/ExecutionSpacesInc.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * @author  Andreas Hadjigeorgiou, The Cyprus Institute,
+ *          Personal-site: https://ahadji05.github.io,
+ *          E-mail: a.hadjigeorgiou@cyi.ac.cy
+ *
+ * @copyright 2022 CaSToRC (The Cyprus Institute), Delphi Consortium (TU Delft)
+ *
+ * @version 1.0
+ *
+ * @section LICENCE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef TMP_EXECUTION_SPACES_INC_HPP
+#define TMP_EXECUTION_SPACES_INC_HPP
+
+#include "ExecutionSpaceSerial.hpp" // host execution space is by default included
+
+#ifdef TMP_ENABLE_CUDA_BACKEND
+#include "ExecutionSpaceOpenMP.hpp"
+#endif
+
+#ifdef TMP_ENABLE_CUDA_BACKEND
+#include "ExecutionSpaceCuda.hpp"
+#endif
+
+#ifdef TMP_ENABLE_HIP_BACKEND
+#include "ExecutionSpaceHip.hpp"
+#endif
+
+#endif
