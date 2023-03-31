43028642cff022f33d653ad16a7dc27d24ce5363
ahadji05 <andreashadji1995@gmail.com>
2023-03-31 10:42:33 +0300

    developed routines for calculating the second-order derivatives pxx and pzz using FD.

---
+++ include/routines/stencil.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file
+ *
+ * @author  Andreas Hadjigeorgiou, The Cyprus Institute,
+ *          Personal-site: https://ahadji05.github.io,
+ *          E-mail: a.hadjigeorgiou@cyi.ac.cy
+ *
+ * @copyright 2022 CaSToRC (The Cyprus Institute), Delphi Consortium (TU Delft)
+ *
+ * @version 1.0
+ *
+ * @section LICENCE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef TMP_STENCIL_HPP
+#define TMP_STENCIL_HPP
+
+#include "containers/ScalarField.hpp"
+#include "execution/ExecutionSpacesInc.hpp"
+#include "types.hpp"
+
+/**
+ * @brief This routine calculates the second-order derivative pxx of a wavefield p using the FD scheme:
+ *
+ *  pxx[z,x] = -1/12*p[z,x-2] +4/3*p[z,x-1] -5/2*p[z,x] +4/3*p[z,x+1] -1/12*p[z,x+2]
+ *
+ * @tparam ExecSpace Execution-Space that is used to resolve the back-end implementation at compile-time.
+ * @tparam MemSpace Memory-Space that must be accessible from the Execution-Space; otherwise compile-time error.
+ * @param pxx wavefield's second-order derivative in x dimension
+ * @param p wavefield from which the derivative pxx is calculated
+ * @param tag tag for dispatching the selection Execution-Space
+ * @return void IF-ONLY the Memory-Space is accessible from the Execution-Space; otherwise it produces compile-time
+ * error.
+ */
+template <class ExecSpace, class MemSpace>
+typename std::enable_if<std::is_same<typename ExecSpace::accessible_space, MemSpace>::value, void>::type fd_pxx(
+    ScalarField<MemSpace> &pxx, const ScalarField<MemSpace> &p, ExecSpace tag);
+
+/**
+ * @brief This routine calculates the second-order derivative pzz of a wavefield p using the FD scheme:
+ *
+ *  pzz[z,x] = -1/12*p[z-2,x] +4/3*p[z-1,x] -5/2*p[z,x] +4/3*p[z+1,x] -1/12*p[z+2,x]
+ *
+ * @tparam ExecSpace Execution-Space that is used to resolve the back-end implementation at compile-time.
+ * @tparam MemSpace Memory-Space that must be accessible from the Execution-Space; otherwise compile-time error.
+ * @param pzz wavefield's second-order derivative in z dimension
+ * @param p wavefield from which the derivative pzz is calculated
+ * @param tag tag for dispatching the selection Execution-Space
+ * @return void IF-ONLY the Memory-Space is accessible from the Execution-Space; otherwise it produces compile-time
+ * error.
+ */
+template <class ExecSpace, class MemSpace>
+typename std::enable_if<std::is_same<typename ExecSpace::accessible_space, MemSpace>::value>::type fd_pzz(
+    ScalarField<MemSpace> &pzz, const ScalarField<MemSpace> &p, ExecSpace tag);
+
+#endif
---
+++ source/cpp/stencil.cpp
@@ -0,0 +1,50 @@
+
+#include "routines/stencil.hpp"
+
+template <>
+void fd_pxx(ScalarField<TMP::MemSpaceHost> &pxx, const ScalarField<TMP::MemSpaceHost> &p, TMP::ExecutionSpaceSerial)
+{
+    assert(pxx.get_nx() == p.get_nx());
+    assert(pxx.get_nz() == p.get_nz());
+
+    size_t nz = pxx.get_nz();
+    size_t nx = pxx.get_nx();
+
+    float_type *pxx_data = pxx.get_ptr();
+    float_type *p_data = p.get_ptr();
+
+    // NOTE FOR POTENTIAL BUG:
+    // IF the range of the loop becomes negative,
+    // for size_t it wrap-arounds to 18446744073709551615!!!
+    assert(nx > 1);
+
+    for (size_t iz(0); iz < nz; ++iz)
+        for (size_t ix(2); ix < nx - 2; ++ix)
+            pxx_data[iz * nx + ix] = -(1 / 12) * p_data[iz * nx + ix - 2] + (4 / 3) * p_data[iz * nx + ix - 1] -
+                                     (5 / 2) * p_data[iz * nx + ix] + (4 / 3) * p_data[iz * nx + ix + 1] -
+                                     (1 / 12) * p_data[iz * nx + ix + 1];
+}
+
+template <>
+void fd_pzz(ScalarField<TMP::MemSpaceHost> &pzz, const ScalarField<TMP::MemSpaceHost> &p, TMP::ExecutionSpaceSerial)
+{
+    assert(pzz.get_nx() == p.get_nx());
+    assert(pzz.get_nz() == p.get_nz());
+
+    size_t nz = pzz.get_nz();
+    size_t nx = pzz.get_nx();
+
+    float_type *pzz_data = pzz.get_ptr();
+    float_type *p_data = p.get_ptr();
+
+    // NOTE FOR POTENTIAL BUG:
+    // IF the range of the loop becomes negative,
+    // for size_t it wrap-arounds to 18446744073709551615!!!
+    assert(nz > 1);
+
+    for (size_t iz(2); iz < nz - 2; ++iz)
+        for (size_t ix(0); ix < nx; ++ix)
+            pzz_data[iz * nx + ix] = -(1 / 12) * p_data[(iz - 2) * nx + ix] + (4 / 3) * p_data[(iz - 1) * nx + ix] -
+                                     (5 / 2) * p_data[iz * nx + ix] + (4 / 3) * p_data[(iz + 1) * nx + ix] -
+                                     (1 / 12) * p_data[(iz + 2) * nx + ix];
+}
