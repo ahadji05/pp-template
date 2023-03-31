7f7651ff7ef1dcf6a92d69cd46d83cbc5718e643
ahadji05 <andreashadji1995@gmail.com>
2023-03-31 14:14:49 +0300

    fixed error that would appear later on when compiling using the GPU back-ends.

--- tests/containers/test_scalarfield.cpp
+++ tests/containers/test_scalarfield.cpp
@@ -8,7 +8,6 @@ TEST(test_constructor_0, GTest_scalarField)
     ASSERT_EQ(a.get_nz(), 0);
     ASSERT_EQ(a.get_nx(), 0);
     ASSERT_EQ(a.get_nElems(), 0);
-    ASSERT_EQ(a.get_nBytes(), 0);
 }
 
 TEST(test_constructor_1, GTest_scalarField)
@@ -18,21 +17,20 @@ TEST(test_constructor_1, GTest_scalarField)
     ASSERT_EQ(a.get_nz(), 6);
     ASSERT_EQ(a.get_nx(), 11);
     ASSERT_EQ(a.get_nElems(), 6 * 11);
-    ASSERT_EQ(a.get_nBytes(), 6 * 11 * sizeof(float_type));
 }
 
-TEST(test_constructor_2, GTest_scalarField)
+TEST(test_constructor_2_explicit_host_container, GTest_scalarField)
 {
     // Check contructor ScalarField(size_t nz, size_t nx, vector_type othervector)
     float_type h_data[] = {float_type(1.0), float_type(2.0), float_type(2.0),
                            float_type(1.0), float_type(0.0), float_type(1.0)};
     vector_type v(h_data, 6);
 
-    ScalarField<memo_space> a(2, 3, v);
+    ScalarField<TMP::MemSpaceHost> a(2, 3, v);
     ASSERT_EQ(a.get_nz(), 2);
     ASSERT_EQ(a.get_nx(), 3);
     for (size_t i(0); i < a.get_nElems(); ++i)
-        ASSERT_EQ(a.get_ptr()[i], h_data[i]);
+        ASSERT_FLOAT_EQ(a.get_ptr()[i], h_data[i]);
 }
 
 TEST(test_copy_constructor, GTest_scalarField)
@@ -46,8 +44,14 @@ TEST(test_copy_constructor, GTest_scalarField)
     ScalarField<memo_space> b(a);
     ASSERT_EQ(b.get_nz(), 2);
     ASSERT_EQ(b.get_nx(), 3);
+
+    float_type *host_data;
+    TMP::MemSpaceHost::allocate(&host_data, 6);
+    memo_space::copyToHost(host_data, b.get_ptr(), 6);
     for (size_t i(0); i < b.get_nElems(); ++i)
-        ASSERT_EQ(b.get_ptr()[i], h_data[i]);
+        ASSERT_EQ(host_data[i], h_data[i]);
+
+    TMP::MemSpaceHost::release(host_data);
 }
 
 TEST(test_copy_assignment, GTest_scalarField)
@@ -61,25 +65,14 @@ TEST(test_copy_assignment, GTest_scalarField)
     ScalarField<memo_space> b = a;
     ASSERT_EQ(b.get_nz(), 1);
     ASSERT_EQ(b.get_nx(), 5);
-    for (size_t i(0); i < b.get_nElems(); ++i)
-        ASSERT_EQ(b.get_ptr()[i], h_data[i]);
-}
 
-TEST(test_copy_assignment_reuse, GTest_scalarField)
-{
-    // Check copy-assignment ScalarField(const ScalarField &otherScalarField)
-    // internally selects the path that avoids deep-copy
-    float_type h_data[] = {float_type(1.6),  float_type(2.2),   float_type(-0.53),
-                           float_type(-1.1), float_type(-0.02), float_type(1.0034)};
-    vector_type v(h_data, 5);
-    ScalarField<memo_space> a(1, 5, v);
-    ScalarField<memo_space> b(5, 1);
-
-    b = a; // b has already an allocated vector with suitable size
-    ASSERT_EQ(b.get_nz(), 1);
-    ASSERT_EQ(b.get_nx(), 5);
+    float_type *host_data;
+    TMP::MemSpaceHost::allocate(&host_data, 6);
+    memo_space::copyToHost(host_data, b.get_ptr(), 6);
     for (size_t i(0); i < b.get_nElems(); ++i)
-        ASSERT_EQ(b.get_ptr()[i], h_data[i]);
+        ASSERT_EQ(host_data[i], h_data[i]);
+
+    TMP::MemSpaceHost::release(host_data);
 }
 
 TEST(test_destructor, GTest_scalarField)
@@ -95,9 +88,5 @@ TEST(test_destructor, GTest_scalarField)
     ASSERT_EQ(b->get_nz(), 1);
     ASSERT_EQ(b->get_nx(), 5);
     ASSERT_EQ(b->get_nElems(), 5);
-    ASSERT_EQ(b->get_nBytes(), 5 * sizeof(float_type));
-    for (size_t i(0); i < b->get_nElems(); ++i)
-        ASSERT_EQ(b->get_ptr()[i], h_data[i]);
-
     ASSERT_NO_THROW(delete b);
 }
