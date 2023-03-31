323a6d17c2625d3717bd52e823588cc0372dc0b6
ahadji05 <andreashadji1995@gmail.com>
2023-03-31 12:23:38 +0300

    script that visualizes the final wavefield on top of the velocity_model's reflectes

---
+++ apps/visualize.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+snapshot = sys.argv[1]
+model = sys.argv[2]
+nz = int(sys.argv[3])
+nx = int(sys.argv[4])
+
+snapshot_image = np.reshape(np.fromfile(snapshot, dtype=np.float32), (nz,nx))
+model_image = np.reshape(np.fromfile(model, dtype=np.float32), (nz,nx))
+model_gradient = ndimage.sobel(model_image, axis=0, mode='constant')
+norm_model_gradient = model_gradient/np.max(np.abs(model_gradient))
+
+vm = np.max(np.abs(snapshot_image))
+
+plt.imshow(norm_model_gradient, cmap='gray')
+plt.imshow(snapshot_image, alpha=0.5, vmin=-vm, vmax=vm, cmap='seismic')
+plt.colorbar()
+plt.title("Snapshot")
+plt.show()
