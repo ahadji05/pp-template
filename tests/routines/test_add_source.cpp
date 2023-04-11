2ca6d6a01c7da67e97c9d5be4457e99281cc2594
ahadji05 <andreashadji1995@gmail.com>
2023-04-11 16:25:55 +0300

    fixed wrong data copy.

--- tests/routines/test_add_source.cpp
+++ tests/routines/test_add_source.cpp
@@ -29,9 +29,9 @@ TEST(test_source_position_value, GTest_add_source)
     add_source(p, 5.5, 5, 1, exec_space()); // apply routine
 
     float_type *host_array;
-    TMP::MemSpaceHost::allocate(&host_array, N);               // allocate a host-array for the unit-test assertions
-    TMP::MemSpaceHost::copyToHost(host_array, p.get_ptr(), N); // copy data from generic container to host_array
-    ASSERT_FLOAT_EQ(host_array[1 * 50 + 5], 5.5);              // source should be added at this particular position
+    TMP::MemSpaceHost::allocate(&host_array, N);        // allocate a host-array for the unit-test assertions
+    memo_space::copyToHost(host_array, p.get_ptr(), N); // copy data from generic container to host_array
+    ASSERT_FLOAT_EQ(host_array[1 * 50 + 5], 5.5);       // source should be added at this particular position
     for (size_t i(0); i < N; ++i)
         if (i != 55)
             ASSERT_FLOAT_EQ(host_array[i], 1.0); // all the rest values should remain unchanged
