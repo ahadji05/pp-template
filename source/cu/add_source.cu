48618b6dd0951079703463ce05e052fa2917ef25
ahadji05 <andreashadji1995@gmail.com>
2023-04-19 11:09:55 +0300

    fixed wrong implementation of add_source kernel

--- source/cu/add_source.cu
+++ source/cu/add_source.cu
@@ -2,12 +2,12 @@
 #include "cuda_config.hpp"
 #include "routines/add_source.hpp"
 
-__global__ void add_source_kernel(float_type *p, float_type src, size_t ix, size_t iz)
+__global__ void add_source_kernel(float_type *p, float_type src, size_t ix, size_t nx, size_t iz)
 {
     size_t i = blockDim.x * blockIdx.x + threadIdx.x;
     if (i > 0)
         return;
-    p[i * iz + ix] = src;
+    p[iz * nx + ix] = src;
 }
 
 #if defined(TMP_ENABLE_CUDA_BACKEND)
@@ -20,8 +20,10 @@ void add_source(ScalarField<TMP::MemSpaceHip> &p, const float_type src, size_t i
 {
     assert(ix < p.get_nx());
     assert(iz < p.get_nz());
+
+    size_t nx = p.get_nx();
 #if defined(TMP_ENABLE_CUDA_BACKEND)
-    add_source_kernel<<<1, 1>>>(p.get_ptr(), src, ix, iz);
+    add_source_kernel<<<1, 1>>>(p.get_ptr(), src, ix, nx, iz);
 #elif defined(TMP_ENABLE_HIP_BACKEND)
     static_assert(false, "NOT IMPLEMENTED YET");
 #endif
