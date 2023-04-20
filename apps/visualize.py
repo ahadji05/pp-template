dca3183ae9e4fb5e9000dad11d3a2aad9eefb5df
ahadji05 <andreash95.AH@gmail.com>
2023-04-20 12:59:31 +0300

    visualize model on the background instead of reflectivity.

--- apps/visualize.py
+++ apps/visualize.py
@@ -11,12 +11,10 @@ nx = int(sys.argv[4])
 float_type = np.float32
 snapshot_image = np.reshape(np.fromfile(snapshot, dtype=float_type), (nz,nx))
 model_image = np.reshape(np.fromfile(model, dtype=float_type), (nz,nx))
-model_gradient = ndimage.sobel(model_image, axis=0, mode='constant')
-norm_model_gradient = model_gradient/np.max(np.abs(model_gradient))
 
 vm = np.max(np.abs(snapshot_image))
 
-plt.imshow(norm_model_gradient, cmap='gray')
+plt.imshow(model_image, cmap='gray')
 plt.imshow(snapshot_image, alpha=0.5, vmin=-vm, vmax=vm, cmap='seismic')
 plt.colorbar()
 plt.title("Snapshot")
