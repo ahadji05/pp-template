f953b944b6411adf23116e30b5a7e6ccabba5cc7
ahadji05 <andreashadji1995@gmail.com>
2023-04-23 12:27:46 +0300

    fixed openmp backend for stencils

--- src/cpp/stencil.cpp
+++ src/cpp/stencil.cpp
@@ -77,12 +77,18 @@ void fd_pxx(ScalarField<TMP::MemSpaceHost> &pxx, const ScalarField<TMP::MemSpace
     // for size_t it wrap-arounds to 18446744073709551615!!!
     assert(nx > 1);
 
-#pragma omp parallel for schedule(static, 10)
+    float_type c0 = -5.0 / 2.0;
+    float_type c1 = 4.0 / 3.0;
+    float_type c2 = -1.0 / 12.0;
+
+#pragma omp parallel for
     for (size_t iz = 0; iz < nz; ++iz)
-        for (size_t ix = 0; ix < nx - 2; ++ix)
-            pxx_data[iz * nx + ix] = -(1 / 12) * p_data[iz * nx + ix - 2] + (4 / 3) * p_data[iz * nx + ix - 1] -
-                                     (5 / 2) * p_data[iz * nx + ix] + (4 / 3) * p_data[iz * nx + ix + 1] -
-                                     (1 / 12) * p_data[iz * nx + ix + 1];
+        for (size_t ix = 2; ix < nx - 2; ++ix)
+        {
+            size_t i = iz * nx + ix;
+            pxx_data[i] =
+                c2 * p_data[i - 2] + c1 * p_data[i - 1] + c0 * p_data[i] + c1 * p_data[i + 1] + c2 * p_data[i + 2];
+        }
 }
 
 template <>
@@ -102,11 +108,15 @@ void fd_pzz(ScalarField<TMP::MemSpaceHost> &pzz, const ScalarField<TMP::MemSpace
     // for size_t it wrap-arounds to 18446744073709551615!!!
     assert(nz > 1);
 
-#pragma omp parallel for schedule(static, 10)
+    float_type c0 = -5.0 / 2.0;
+    float_type c1 = 4.0 / 3.0;
+    float_type c2 = -1.0 / 12.0;
+
+#pragma omp parallel for
     for (size_t iz = 2; iz < nz - 2; ++iz)
         for (size_t ix = 0; ix < nx; ++ix)
-            pzz_data[iz * nx + ix] = -(1 / 12) * p_data[(iz - 2) * nx + ix] + (4 / 3) * p_data[(iz - 1) * nx + ix] -
-                                     (5 / 2) * p_data[iz * nx + ix] + (4 / 3) * p_data[(iz + 1) * nx + ix] -
-                                     (1 / 12) * p_data[(iz + 2) * nx + ix];
+            pzz_data[iz * nx + ix] = c2 * p_data[(iz - 2) * nx + ix] + c1 * p_data[(iz - 1) * nx + ix] +
+                                     c0 * p_data[iz * nx + ix] + c1 * p_data[(iz + 1) * nx + ix] +
+                                     c2 * p_data[(iz + 2) * nx + ix];
 }
 #endif
